f400cb32d82e91f9807fb591787d96706838b0ea
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-01-15 12:14:01 -0400

    fix: fix fresh initialization for database in mongo container

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,18 +1,13 @@
-#!/bin/bash
-set -e
-
 mongosh <<EOF
-use admin
-db.createUser(
-  {
-    user: "${DB_USER}",
-    pwd: "${DB_PASSWORD}",
-    roles: [
-      {
-        role: "readWrite",
-        db: "${DB_NAME}"
-      }
-    ]
-  }
-);
+use $DB_NAME
+
+db.createUser({
+  user: '$DB_USER',
+  pwd: '$DB_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$DB_NAME'
+  }]
+})
+
 EOF
